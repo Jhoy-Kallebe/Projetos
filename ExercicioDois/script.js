82d2780b30bbdb26d612e433f47de909dc7dcd1c
Jhoy-Kallebe <kallebejhoy@gmail.com>
2023-01-05 17:27:49 -0300

    final

--- ExercicioDois/script.js
+++ ExercicioDois/script.js
@@ -1,1 +1,43 @@
-alert('olá')
+function verificar(){
+    var data = new Date;
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtAno');
+    var res = document.querySelector('#res');
+
+    if(fano.value.legth == 0 || fano.value > ano){
+        alert('Verifique os dados');
+    } else {
+        var sex = document.getElementsByName('radSex');
+        var idade = ano - Number(fano.value);
+        var genero = ''
+        var img = document.createElement('img')
+
+        if(sex[0].checked){
+            genero = 'Homem';
+            if(idade >= 0 && idade <10){
+                img.setAttribute('src', 'bebeHomem.jpg');
+            } else if(idade < 21){
+                img.setAttribute('src', 'jovemHomem.jpg');
+            } else if (idade < 50){
+                img.setAttribute('src', 'adultoHomem.jpg');
+            }else {
+                img.setAttribute('src', 'idosoHomem.jpg');
+            }
+        } else if(sex[1].checked){
+            genero = 'Mulher';
+            if(idade >= 0 && idade <10){
+                img.setAttribute('src', 'bebeMulher.jpg');
+            } else if(idade < 21){
+                img.setAttribute('src', 'jovemMulher.jpg');
+            } else if (idade < 50){
+                img.setAttribute('src', 'adultoMulher.jpg');
+            }else {
+                img.setAttribute('src', 'idosoMulher.jpg');
+            }
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+        res.appendChild(img);
+
+    }
+}
